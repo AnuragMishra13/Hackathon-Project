68b5cb215d4cf5d0373d450bc106db7a0d547e56
Varad Ijantkar <101548183+VaradIjantkar@users.noreply.github.com>
2023-05-07 11:39:50 +0530

    Now Login and Signup redirection working

--- models/user.js
+++ models/user.js
@@ -2,16 +2,29 @@ const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
 
+    name:{
+        type: String,
+        trim:true,
+        required:true 
+    },
+    surname:{
+        type: String,
+        trim:true,
+        required:true
+    },
     username:{
         type: String,
-        required:true
+        trim:true,
+        required:[true,'']
     },
     password:{
         type:String,
+        trim:true,
         required:true
     },
     email:{
         type:String,
+        trim:true,
         required:true
     }
 }, {timestamps : true });

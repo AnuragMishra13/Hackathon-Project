d45bca6a343632b5c535d45f93d60e232f396fcf
CryonBeast <115824470+CryonBeast@users.noreply.github.com>
2023-05-08 12:35:18 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const path = require("path");
 const usersRoutes = require("../routes/userRoutes");
-
 const PORT = process.env.PORT || 3000; 
 
-dotenv.config();
-
 app.use('/users',usersRoutes)
-
 app.use(express.static(path.join(__dirname,"../public")))
-
 app.set("view engine","ejs");
 
 app.get("/home",(req,res)=>{
@@ -21,6 +16,10 @@ app.get("/home",(req,res)=>{
 
 app.get("/editor",(req,res)=>{
     res.render("editor");
+})
+
+app.post("/editor",(req,res)=>{
+    alert("hi");
 })
 
 mongoose.connect(process.env.MONGO_URL)

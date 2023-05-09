466d992e617c1618c6ab690f6c897f9e8e7f719d
Varad Ijantkar <101548183+VaradIjantkar@users.noreply.github.com>
2023-05-09 12:00:00 +0530

    Design revamp, login popup, signup popup

---
+++ public/app.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper')
+const loginlink = document.querySelector('.login-link')
+const signuplink = document.querySelector('.signup-link')
+const loginpop = document.querySelector('.btnLogin')
+const closebtn = document.querySelector('.icon-close')
+const signuppop = document.querySelector('.btnSignUp')
+
+signuplink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+})
+
+loginlink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+})
+
+loginpop.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+
+closebtn.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+})

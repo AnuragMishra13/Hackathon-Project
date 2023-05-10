66be204788d763d1072de0cc572fa213a50e77a0
Varad Ijantkar <101548183+VaradIjantkar@users.noreply.github.com>
2023-05-10 14:11:28 +0530

    Some bug fixes

--- public/app.js
+++ public/app.js
@@ -2,9 +2,8 @@ const wrapper = document.querySelector('.wrapper')
 const loginlink = document.querySelector('.login-link')
 const signuplink = document.querySelector('.signup-link')
 const loginpop = document.querySelector('.btnLogin')
+const signuppop = document.querySelector('.btnSignup')
 const closebtn = document.querySelector('.icon-close')
-const signuppop = document.querySelector('.btnSignUp')
-
 
 let text = document.getElementById('text')
 let sky = document.getElementById('sky')
@@ -14,34 +13,36 @@ let backtree = document.getElementById('backtree')
 let mountain = document.getElementById('mountain')
 
 signuplink.addEventListener('click', () => {
-    window.history.pushState(null,null,"/signup")
+    window.history.pushState(null, null, "/signup")
     wrapper.classList.add('active');
 })
 
 loginlink.addEventListener('click', () => {
-    window.history.pushState(null,null,"/login")
+    window.history.pushState(null, null, "/login")
     wrapper.classList.remove('active');
 })
 
-// signuppop.addEventListener('click',()=>{
-//     wrapper.classList.add('active-popup');
-// })
+signuppop.addEventListener('click', () => {
+    window.history.pushState(null, null, "/signup")
+    wrapper.classList.add('active-popup');
+    wrapper.classList.add('active');
+})
 
 loginpop.addEventListener('click', () => {
-    window.history.pushState(null,null,"/login")
+    window.history.pushState(null, null, "/login")
     wrapper.classList.add('active-popup');
+    wrapper.classList.remove('active');
 })
 
 closebtn.addEventListener('click', () => {
-    window.history.pushState(null,null,"/home")
+    window.history.pushState(null, null, "/home")
     wrapper.classList.remove('active-popup');
 })
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-
-    text.style.marginTop = value * 2.5 + 'px';
-
+    text.style.marginTop = value * 1.5 + 'px';
+    wrapper.style.marginTop = value * 0.5 + 'px';
 
     righttree.style.marginRight = value * -1.5 + 'px';
     righttree.style.marginBottom = value * -1.5 + 'px';

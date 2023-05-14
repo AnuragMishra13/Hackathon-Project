b72fe443237ccfbdd9bf01175c1fc60580bdd15a
Varad Ijantkar <101548183+VaradIjantkar@users.noreply.github.com>
2023-05-14 21:08:06 +0530

    Editor

--- public/editor-javascript/editor.js
+++ public/editor-javascript/editor.js
@@ -1,126 +1,140 @@
-const navbar = () => {
-  var isNav = document
-    .getElementById("sample")
-    .contentWindow.document.getElementsByClassName("navbar");
-  if (isNav.length == 0) {
-    var c1 = document.getElementById("sample").contentWindow;
-    var tag = c1.document.createElement("nav");
-    tag.className = "navbar navbar-expand-lg bg-body-tertiary";
-    tag.innerHTML = `<div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>`;
-    var element = c1.document.getElementById("new");
-    element.appendChild(tag);
-  } else {
-    alert("Nav present");
-  }
-};
+// const navbar = () => {
+//   var isNav = document
+//     .getElementById("sample")
+//     .contentWindow.document.getElementsByClassName("navbar");
+//   if (isNav.length == 0) {
+//     var c1 = document.getElementById("sample").contentWindow;
+//     var tag = c1.document.createElement("nav");
+//     tag.className = "navbar navbar-expand-lg bg-body-tertiary";
+//     tag.innerHTML = `<div class="container-fluid">
+//       <a class="navbar-brand" href="#">Navbar</a>
+//       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//       </button>
+//       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+//           <li class="nav-item">
+//             <a class="nav-link active" aria-current="page" href="#">Home</a>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link" href="#">Link</a>
+//           </li>
+//           <li class="nav-item dropdown">
+//             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//               Dropdown
+//             </a>
+//             <ul class="dropdown-menu">
+//               <li><a class="dropdown-item" href="#">Action</a></li>
+//               <li><a class="dropdown-item" href="#">Another action</a></li>
+//               <li><hr class="dropdown-divider"></li>
+//               <li><a class="dropdown-item" href="#">Something else here</a></li>
+//             </ul>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link disabled">Disabled</a>
+//           </li>
+//         </ul>
+//         <form class="d-flex" role="search">
+//           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+//           <button class="btn btn-outline-success" type="submit">Search</button>
+//         </form>
+//       </div>
+//     </div>`;
+//     var element = c1.document.getElementById("new");
+//     element.appendChild(tag);
+//   } else {
+//     alert("Nav present");
+//   }
+// };
 
-const accordian = () => {
-  var isAcc = document
-    .getElementById("sample")
-    .contentWindow.document.getElementsByClassName("accordian");
-  if (isAcc.length === 0) {
-    var c2 = document.getElementById("sample").contentWindow;
-    var acc = c2.document.createElement("div");
-    acc.className = "accordion";
-    acc.id = "accordionExample";
-    acc.innerHTML = `<div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>`;
-    var element1 = c2.document.getElementById("new");
-    element1.appendChild(acc);
-  } else {
-    alert("Accordian present");
-  }
-};
+// const accordian = () => {
+//   var isAcc = document
+//     .getElementById("sample")
+//     .contentWindow.document.getElementsByClassName("accordian");
+//   if (isAcc.length === 0) {
+//     var c2 = document.getElementById("sample").contentWindow;
+//     var acc = c2.document.createElement("div");
+//     acc.className = "accordion";
+//     acc.id = "accordionExample";
+//     acc.innerHTML = `<div class="accordion-item">
+//     <h2 class="accordion-header">
+//       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+//         Accordion Item #1
+//       </button>
+//     </h2>
+//     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+//       <div class="accordion-body">
+//         <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+//       </div>
+//     </div>
+//   </div>
+//   <div class="accordion-item">
+//     <h2 class="accordion-header">
+//       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+//         Accordion Item #2
+//       </button>
+//     </h2>
+//     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+//       <div class="accordion-body">
+//         <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+//       </div>
+//     </div>
+//   </div>
+//   <div class="accordion-item">
+//     <h2 class="accordion-header">
+//       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+//         Accordion Item #3
+//       </button>
+//     </h2>
+//     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+//       <div class="accordion-body">
+//         <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+//       </div>
+//     </div>
+//   </div>`;
+//     var element1 = c2.document.getElementById("new");
+//     element1.appendChild(acc);
+//   } else {
+//     alert("Accordian present");
+//   }
+// };
 
-const heading = () => {
-  var c1 = document.getElementById("sample").contentWindow;
-  var heading = c1.document.createElement("h1");
-  var text = prompt("Enter the text : ");
-  heading.innerHTML = text;
-  var element = c1.document.getElementById("new");
-  element.appendChild(heading);
-};
+// const heading = () => {
+//   var c1 = document.getElementById("sample").contentWindow;
+//   var heading = c1.document.createElement("h1");
+//   var text = prompt("Enter the text : ");
+//   heading.innerHTML = text;
+//   var element = c1.document.getElementById("new");
+//   element.appendChild(heading);
+// };
 
-const text = () => {
-  var c1 = document.getElementById("sample").contentWindow;
-  var text = c1.document.createElement("p");
-  var innertext = prompt("Enter the text : ");
-  text.innerHTML = innertext;
-  var element = c1.document.getElementById("new");
-  element.appendChild(text);
-};
-const panel = () => {
-  document.querySelector(".wrapper").classList.toggle("side-panel-open");
-};
+// const text = () => {
+//   var c1 = document.getElementById("sample").contentWindow;
+//   var text = c1.document.createElement("p");
+//   var innertext = prompt("Enter the text : ");
+//   text.innerHTML = innertext;
+//   var element = c1.document.getElementById("new");
+//   element.appendChild(text);
+// };
+// const panel = () => {
+//   document.querySelector(".wrapper").classList.toggle("side-panel-open");
+// };
 
-const htmlcode = async() => {
-  var c1 = document.getElementById("sample").contentWindow;
-  console.log(c1.document)
-};
+// const htmlcode = async() => {
+//   var c1 = document.getElementById("sample").contentWindow;
+//   console.log(c1.document)
+// };
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
